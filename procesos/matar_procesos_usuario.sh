0391c07018f73c6af9ece5ed3b552f66ce607746
Damian Piazza <83801199+XkaunanX@users.noreply.github.com>
2025-05-01 02:17:43 -0300

    Delete procesos directory

--- procesos/matar_procesos_usuario.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Termina todos los procesos del usuario pasado como parámetro
-pkill -u "$1"  # Mata todos los procesos de un usuario
-echo "Todos los procesos de '$1' han sido terminados."
